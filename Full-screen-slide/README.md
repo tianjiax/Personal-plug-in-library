0d088520edfd8e61ee574f1cfd6547359b938974
tianjiax <1044880118@qq.com>
2018-01-05 09:29:51 +0800

    代码升级
    添加多个参数及对README文件进行补充

--- Full-screen-slide/README.md
+++ Full-screen-slide/README.md
@@ -1,46 +1,78 @@
-### 全屏轮播图
-> 该插件基于jquery开发，现阶段很多PC网站都需要用到幻灯片切换的效果，全屏幻灯片也是一种趋势，而且很多时候要在页面中复用该幻灯片，而且对于PC用户而言，IE8+用户居多，我们需要做一个兼容该用户群体的全屏幻灯片。这时候该插件便应运而生，我们可以通过简单的调用及样式来对其进行调用，从而完成快速开发的过程。该模块并非只能是轮播图，轮播图条目区域可以放上文字列表、图片等任意内容，这块在于你自己选择。接下来我们对该插件进行一个解析：
+## 全屏轮播图
+> 该插件基于jquery开发，现阶段很多PC网站都需要用到轮播图切换的效果，很多时候要在页面中复用轮播图，而且对于PC用户而言，IE8+用户居多，我们需要做一个兼容该用户群体的全屏轮播图，同时我们也可以通过参数设置来把该幻灯片设置为非全屏复用多个位置。这时候该插件便应运而生。我们可以通过简单的调用及样式来对其进行调用，从而完成快速开发的过程。该模块并非只能是轮播图，轮播图条目区域可以放上文字列表、图片等任意内容，这块在于你自己选择。接下来我们对该插件进行一个解析：
 
 ##### [jquery封装插件的方法](http://blog.csdn.net/osdfhv/article/details/53185914)
+
+### demo预览
+![image](http://ww1.sinaimg.cn/large/0060lm7Tly1fn4lgv6n98j31gu0fc4k8.jpg)
+
+### 方法调用
+> 现在对方法调用进行一个简单的解析，后面再就函数进行具体的解析。
 
 ##### html
 ```html
 <!-- 重置样式表 -->
 <link rel="stylesheet" href="https://necolas.github.io/normalize.css/4.1.1/normalize.css">
-<!-- slide样式表 -->
-<link rel="stylesheet" href="css/full-screen-slide.css">
+<!-- 引入slide插件样式表 -->
+<link rel="stylesheet" href="css/full-screen-slide.min.css">
 <!-- html模块 -->
-<div class="w-auto-Center" style="width:1200px">
-	<div class="slide-box slide-box1" style="display: block;">
-		<div class="slide-con">
-			<div class="slide-out">
-				<div class="slide-li act"><img src="http://tg.shanbane.com/tianjia_cesv3/images/temp/main2/slide/1.png" alt=""></div>
-				<div class="slide-li"><img src="http://tg.shanbane.com/tianjia_cesv3/images/temp/main2/slide/2.png" alt=""></div>
-				<div class="slide-li"><img src="http://tg.shanbane.com/tianjia_cesv3/images/temp/main2/slide/3.png" alt=""></div>
-				<div class="slide-li"><img src="http://tg.shanbane.com/tianjia_cesv3/images/temp/main2/slide/4.png" alt=""></div>
-				<div class="slide-li"><img src="http://tg.shanbane.com/tianjia_cesv3/images/temp/main2/slide/5.png" alt=""></div>
+<div class="slide-box slide-box-ex">
+	<!-- 轮播图主体模块 -->
+	<div class="slide-con">
+		<div class="slide-out">
+			<div class="slide-li"><img src="images/slide/1.png" style="width: 100%" alt=""></div>
+			<div class="slide-li"><img src="images/slide/2.png" style="width: 100%" alt=""></div>
+			<div class="slide-li">
+				<div class="slide-li-content">
+					<div class="box" style="background: #ddd;color: #333;height: 600px">这里是文本存放位置</div>
+				</div>
 			</div>
 		</div>
-		<div class="slide-page">
-			<div class="page-l"><img src="http://tg.shanbane.com/tianjia_cesv3/images/icon/page-l.png" alt=""></div>
-			<div class="page-r"><img src="http://tg.shanbane.com/tianjia_cesv3/images/icon/page-r.png" alt=""></div>
-		</div>
+	</div>
+	<!-- 自定义上一页下一页 -->
+	<div class="slide-page">
+		<div class="slide-button-prev"><i></i></div>
+		<div class="slide-button-next"><i></i></div>
 	</div>
 </div>
 <!-- 引入slide插件js文件 -->
-<script src="js/full-screen-slide.js"></script>
+<script src="js/full-screen-slide.min.js"></script>
 ```
 
 ##### js
 ```js
-$('.slide-box').setSlide({
+// 参数调用
+$('.slide-box-ex').setSlide({
+	isFull:false,
+	switchingSpeed:400,
 	slideTime: 3000,
-	autoPlay:true
+    autoPlay:true,
+	prevEl:$('.slide-box-ex').find('.slide-button-prev'),
+	nextEl:$('.slide-box-ex').find('.slide-button-next'),
+	pagination:true,
+	pagingTrigger:'hover',
+	slideLiw:1100,
+	slideLih:540,
+	fn:function(index) {
+		console.log(index)
+	}
 });
 ```
-##### 参数解析
+### 参数解析
 参数 | 类型 |说明 | 示例值
 ---|---|---|---
-slideTime | number | 定义幻灯片自动播放的毫秒数，只有开启autoPlay属性时有效。如若为空，默认自动播放时间为2000毫秒 | 3000
-autoPlay | boolean | 是否自动切换 		| true
+isFull 			| boolean 	| （非必填）定义幻灯片是否为全屏展示该幻灯片。默认为true | false
+switchingSpeed 	| number 	| （非必填）定义轮播图切换速度的毫秒数。如若为空，默认自动播放时间为500毫秒	| 600
+slideTime 		| number 	| （非必填）定义轮播图自动播放的毫秒数，只有开启autoPlay属性时有效。如若为空，默认自动播放时间为2000毫秒 | 3000
+autoPlay 		| boolean 	| （非必填）是否自动切换，默认为false | true
+prevEl 			| DOM 		| （非必填）点击轮播图切换到上一张，可自定义标签，Dom节点操作沿袭jquery的Dom节点操作，默认为$('#slide-button-prev')。如果第一张操作时候便跳到最后一张。 | $('.slide-button-prev')
+nextEl 			| DOM 		| （非必填）点击轮播图切换到下一张，可自定义标签，Dom节点操作沿袭jquery的Dom节点操作，默认为$('#slide-button-next')。如果最后一张操作时候便跳到第一张。 | $('.slide-button-prev')
+pagination 		| boolean 	| （非必填）是否使用分页导航，只有开启autoPlay属性时有效。默认为false | true
+pagingTrigger	| string 	| （非必填）分页导航指示器的触发事件，默认为click | 'hover'
+slideLiw 		| number 	| （非必填）设定轮播容器宽度，默认单位为px，无需加单位。默认为1200 | 1000
+slideLih 		| number 	| （非必填）设定轮播容器高度，默认单位为px，无需加单位。默认为600 | 500
+fn				| Object	| （非必填） 回调函数，我们可以在当轮播图切换时，拿到当前轮播图的索引值并在该时刻进行回调的操作。与一般函数写法相同，索引值可传可不传。| function(index) {} 或 function() {} 
+
+### 结语
+> 在前端项目中，我们需要用到轮播图的地方很多，因为每次都是重复性的编写导致个人时间浪费，现阶段我们将其封装为一个插件，只要按照一定规则便可快速开发该模块。本人才疏学浅，简单对日常使用的幻灯片操作进行了一个封装，如若有好的想法欢迎提出来，共勉。 -- 钿佳
 
--- Full-screen-slide/demo/js/full-screen-slide.js
+++ Full-screen-slide/demo/js/full-screen-slide.js
@@ -1,55 +1,108 @@
 (function ($) { 
     $.fn.setSlide = function(option){ 
         var set = $.extend({ 
-            oSlide: $(this), 
+            Slide: $(this), 
+            switchingSpeed:500,
+            isFull:true,
             slideTime: 2000,
             autoPlay:false,
-            nextEl:$('#slide-button-prev'),
-            prevEl:$('#slide-button-next'),
-            pagination:false
+            prevEl:$('#slide-button-prev'),
+            nextEl:$('#slide-button-next'),
+            pagination:false,
+            pagingTrigger:'click',
+            slideLiw:1200,
+            slideLih:600,
+            fn:function(){
+
+            }
         } ,option);
 
         return this.each(
         	function(){
-				var oSlideIndex 	=	0;
-				var oSlide 			=	set.oSlide;
-				var slide_out		=	set.oSlide.find('.slide-out');
-				var slide_box_li 	= 	set.oSlide.find('.slide-li');
-				var slidePrev 		= 	set.nextEl;
-				var slideNext 		= 	set.prevEl;
-				var pagination 		=   set.pagination;
-				var slideBullet 	= 	set.oSlide.find('.slide-pagination-bullet');
-				var slideTime 		= 	set.slideTime;
-				var len 			=	slide_box_li.length;
+        		// 参数赋值
+				var SlideIndex 	=	0,
+					Slide 			=	set.Slide,
+					switchingSpeed 	=	set.switchingSpeed,
+					isFull 			=	set.isFull,
+					slideLiw		= 	set.slideLiw,
+					slideLih		= 	set.slideLih,
+					slidePrev 		= 	set.prevEl,
+					slideNext 		= 	set.nextEl,
+					pagination 		=   set.pagination,
+					pagingTrigger	=	set.pagingTrigger,
+					fn 				=	set.fn,
+					slideOut		=	Slide.find('.slide-out'),
+					slideLi 		= 	Slide.find('.slide-li'),
+					slideBullet 	= 	Slide.find('.slide-pagination-bullet'),
+					slideTime 		= 	set.slideTime,
+					slideLen 		=	slideLi.length;
 
+				// 轮播图中间主体宽高
+				Slide.css({'width':slideLiw,'height':slideLih});
+				slideLi.css({'width':slideLiw,'height':slideLih});
+
+				// 轮播图外层宽高
+				slideOut.css({'width':slideLiw*slideLen,'height':slideLih});
+
+				// 默认第一个选中
+				slideLi.eq(0).addClass('slide-li-act');
+
+				// 是否全屏
+				if (!isFull) {
+					Slide.css({'overflow':'hidden'});
+					slideOut.addClass('isfull')
+				}
+
+				// 上一页
 				slidePrev.click(function(){
-					oSlideIndex --;
-					if(oSlideIndex<0){
-						oSlideIndex=len-1;
+					SlideIndex --;
+					if(SlideIndex<0){
+						SlideIndex=slideLen-1;
 					}
-					slide(oSlideIndex)
+					slide(SlideIndex)
 				})
 
+				// 下一页
 				slideNext.click(function(){
-					oSlideIndex ++;
-					if(oSlideIndex>len-1){
-						oSlideIndex=0;
+					SlideIndex ++;
+					if(SlideIndex>slideLen-1){
+						SlideIndex=0;
 					}
-					slide(oSlideIndex)
+					slide(SlideIndex)
 				})
 
+				// 添加分页导航
+				if (pagination) {
+					Slide.append("<div class='slide-pagination'></div>");
+					for (var i = 0; i < slideLen; i++) {
+						Slide.find('.slide-pagination').append("<span class='slide-pagination-bullet'></span>")
+					}
+					Slide.find('.slide-pagination .slide-pagination-bullet').eq(0).addClass('slide-pagination-bullet-active');
+					var slideBullet 	= 	Slide.find('.slide-pagination-bullet');
+				}
+
+				// 分页导航
+				if (pagingTrigger == 'hover') {
+					var pagingTrigger = "mouseover";
+				}
+				slideBullet.on(pagingTrigger,function(){
+					SlideIndex  = $(this).index();
+					slide(SlideIndex);
+				})
+
+				// 自动播放
 				if (set.autoPlay) {
 					function autoPlayFun(){
-						oSlideIndex ++;
-						if(oSlideIndex>len-1){
-							oSlideIndex=0;
+						SlideIndex ++;
+						if(SlideIndex>slideLen-1){
+							SlideIndex=0;
 						}
-						slide(oSlideIndex)
+						slide(SlideIndex)
 					}
 
 					var autoPlaySet = setInterval(autoPlayFun,slideTime);
 
-					oSlide.hover(
+					Slide.hover(
 						function(){
 							clearInterval(autoPlaySet)
 						},
@@ -59,24 +112,13 @@
 					)
 				}
 
-				if (pagination) {
-					for (var i = 0; i < len; i++) {
-						oSlide.find('.slide-pagination').append("<span class='slide-pagination-bullet'></span>")
-					}
-					oSlide.find('.slide-pagination .slide-pagination-bullet').eq(0).addClass('slide-pagination-bullet-active');
-					var slideBullet 	= 	oSlide.find('.slide-pagination-bullet');
-					slideBullet.click(function(){
-						var oSlideIndex  = $(this).index();
-						slide(oSlideIndex);
-						return oSlideIndex
-					});
-				}
-
-				function slide(oSlideIndex){
-					var oSlideIndex = oSlideIndex;
-					slide_out.stop().animate({'margin-left': '-'+1200*oSlideIndex},800);
-					slide_box_li.eq(oSlideIndex).addClass('act').siblings().stop().removeClass('act');
-					slideBullet.eq(oSlideIndex).addClass('slide-pagination-bullet-active').siblings().stop().removeClass('slide-pagination-bullet-active');
+				// 动画执行事件
+				function slide(SlideIndex){
+					slideOut.stop().animate({'margin-left': '-'+slideLiw*SlideIndex},switchingSpeed);
+					slideLi.eq(SlideIndex).addClass('slide-li-act').siblings().stop().removeClass('slide-li-act');
+					slideBullet.eq(SlideIndex).addClass('slide-pagination-bullet-active').siblings().stop().removeClass('slide-pagination-bullet-active');
+					fn && fn(SlideIndex);
+					return SlideIndex;
 				}
 			}
         )
