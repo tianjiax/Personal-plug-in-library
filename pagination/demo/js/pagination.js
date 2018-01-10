fab3e3a6af05e4013c3fd6846f7d41cfa91c7b3e
tianjiax <1044880118@qq.com>
2018-01-10 17:17:44 +0800

    分页代码补充

--- pagination/demo/js/pagination.js
+++ pagination/demo/js/pagination.js
@@ -4,10 +4,11 @@
 			paginationBox	: $(this),
 			count			: '',
 			limit			: 10,
+			groups			: 5,
 			curr			: 1,
 			prev			: '上一页',
 			next			: '下一页',
-			jump			: ''
+			fn				: function(){}
 		},option)
 
 		return this.each(
@@ -17,24 +18,67 @@
 				var paginationBox 	= set.paginationBox,
 					count 			= set.count,
 					limit		 	= set.limit,
+					groups		 	= set.groups,
 					curr 			= set.curr,
 					prev 			= set.prev,
 					next 			= set.next,
-					jump			= set.jump;
+					fn				= set.fn;
 
 				// 通过set计算参数	
-				var paginalNumber	= count/limit;
+				var paginalNumber	= count/limit,
+					index			= 0;
 
 				// 插入上下页
 				paginationBox.append("<a href='javascript:;' class='ui-page-pre'>" + prev + "</a><span class='ui-page-box'></span><a href='javascript:;' class='ui-page-next'>" + next +  "</a>");
 
 				// 插入页码
-				for (var i = 0; i < paginalNumber; i++) {
+				for (var i = 0; i < groups; i++) {
 					paginationBox.find('.ui-page-box').append('<a href="javascript:;" class="ui-page-number" data-number=' + (i+1) + '>' + (i+1) + '</a>')
 				};
 
 				// 默认第一个选中
-				paginationBox.find('.ui-page-number').eq(0).addClass('ui-page-act')
+				paginationBox.find('.ui-page-number').eq(curr-1).addClass('ui-page-act');
+
+				// 点击事件
+				paginationBox.find('.ui-page-number').click(
+					function(){
+						index = $(this).index();
+						pageFn(index)
+					}
+				)
+
+				// 上一页
+				paginationBox.find('.ui-page-pre').click(
+					function(){
+						index--;
+						if ( index < 0) {
+							index = 0
+						};
+						pageFn(index)
+					}
+				)
+
+				// 下一页
+				paginationBox.find('.ui-page-next').click(
+					function(){
+						index++;
+						if ( index > paginalNumber-1) {
+							index = paginalNumber-1;
+						};
+						pageFn(index)
+					}
+				)
+
+
+				// 执行函数
+				function pageFn(index){
+
+					paginationBox.find('.ui-page-number').eq(index).addClass('ui-page-act').siblings().removeClass('ui-page-act');
+
+					curr = index +1;
+
+					fn && fn(curr);
+				}
 
 			}
 		)
